9418d50bb6aa799dfe57c7cd4c899003aa83d9fc
root <root@bioinf-ssmotif.biotec.tu-dresden.de>
2024-01-24 16:44:00 +0100

    Remove debugging from template script

--- proxy/cellxgene/etc/cellxgene_templates/mkcellxgene_config.pl
+++ proxy/cellxgene/etc/cellxgene_templates/mkcellxgene_config.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 use 5.20.0;
 use warnings;
-use Data::Dumper;
 
 my $group = $ARGV[0] // die "need a group name\n";
 
@@ -10,7 +9,6 @@ my $APACHECONFDIR = $ARGV[2] // '/etc/apache2/sites-available';
 my $WEBROOT = $ARGV[3] // '/var/www';
 
 my $apache_template = $TEMPLATEDIR. '/apache.conf';
-say "Apache template = $apache_template";
 my $mapping = $TEMPLATEDIR. '/group_host_mapping';
 
 my $dstconf = $APACHECONFDIR. '/cellxgene/'."$group".'.conf';
@@ -22,27 +20,21 @@ my @content;
 sub readtemplate {
     my $file = shift;
     @content = ();
-    say "File = $file";
     open (my $fd, "<", $file) or die "Can't open file $file, $!";
     while (<$fd>) {
 	    next if $_ =~ m/^\#/;
-	    say $_;
 	    push @content,$_;
     }
     close $fd;
-    #say Dumper(@content);
     return @content if @content;
     return;
 }
 
 my @match = grep {/$group:/} readtemplate($mapping);
-say Dumper(@match);
 die "fix your mapping: $mapping, found multiple matches!\n" if scalar(@match) > 1;
 die "fix your mapping: $mapping, found zero matches!\n" if scalar(@match) == 0;
 my $host = (split(/:/,$match[0]))[1];
 my $port = (split(/:/,$match[0]))[2];
-say "port = $port";
-say "host = $host";
 die "Can't find port for group $group" if ! $port;
 die "Can't find host for group $group" if ! $host;
 
@@ -89,8 +81,3 @@ sub update_index{
 
 write_conf($dstconf,gen_conf($apache_template));
 write_conf($index,update_index($index));
-
-my $error = `systemctl daemon-reload`;
-$error = `service apache2 reload`;
-say "$error" if $error;
-say " - add cellxgeneUser to the new $group";
